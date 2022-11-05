4a3397f266404b4ad039a459c4eba7eb5ba58122
Juliansci <julian.scialabba@gmail.com>
2022-11-04 23:36:17 -0300

    Test husky checking pre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [aVar, setAVar] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (aVar) {
-      console.log(aVar);
-    }
-    setAVar('aca');
-  }, []);
-
   return <div className="App">App</div>;
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  <React.StrictMode></React.StrictMode>,
+  document.getElementById('root'),
 );
